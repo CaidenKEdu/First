ba28e62be05b6d160cbee7677404386a42ae98a1
ckunitsky <ckunitsky@thegraysonschool.org>
2023-09-26 14:55:59 -0400

    Changed From Replit to Pycharm Formatting

--- main.py
+++ main.py
@@ -2,32 +2,32 @@ import csv
 from time import perf_counter
 
 
-def Fact(n):
-  if n > 0:
-    return n*Fact(n-1)
-  else:
-    return 1
+def fact(n):
+    if n > 0:
+        return n*fact(n-1)
+    else:
+        return 1
 
 
 def fib_bad(n):
-  if n > 1:
-    return fib_bad(n-1) + fib_bad(n-2)
-  else:
-    return 1
+    if n > 1:
+        return fib_bad(n-1) + fib_bad(n-2)
+    else:
+        return 1
 
 
 def main():
-  with open("fib_bad.csv", 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    for i in range(35):
-      exe_time = perf_counter()
-      val = fib_bad(i)
-      exe_time = perf_counter() - exe_time
-      exe_time2 = perf_counter()
-      val2 = Fact(i)
-      exe_time2 = perf_counter() - exe_time2
-      csvwriter.writerow([i, val, exe_time, val2, exe_time2])
+    with open("fib_bad.csv", 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for i in range(35):
+            exe_time = perf_counter()
+            val = fib_bad(i)
+            exe_time = perf_counter() - exe_time
+            exe_time2 = perf_counter()
+            val2 = fact(i)
+            exe_time2 = perf_counter() - exe_time2
+            csvwriter.writerow([i, val, exe_time, val2, exe_time2])
 
 
 if __name__ == "__main__":
-  main()
+    main()
