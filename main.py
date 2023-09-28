a021f7d8bc95cd67c2ae5012db5423ea166d0c88
ckunitsky <ckunitsky@thegraysonschool.org>
2023-09-28 14:27:40 -0400

    Implemented fib_good

--- main.py
+++ main.py
@@ -16,18 +16,31 @@ def fib_bad(n):
         return 1
 
 
+def fib_good(n):
+    a = 0
+    b = 0
+    c = 1
+    for i in range(n):
+        a = c + b
+        b = c
+        c = a
+    return c
+
+
 def main():
-    with open("fib_bad.csv", 'w', newline='') as csvfile:
+    with open("data.csv", 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         for i in range(35):
             exe_time = perf_counter()
-            val = fib_bad(i)
+            val = fact(i)
             exe_time = perf_counter() - exe_time
             exe_time2 = perf_counter()
-            val2 = fact(i)
+            val2 = fib_bad(i)
             exe_time2 = perf_counter() - exe_time2
-            csvwriter.writerow([i, val, exe_time, val2, exe_time2])
-
+            exe_time3 = perf_counter()
+            val3 = fib_good(i)
+            exe_time3 = perf_counter() - exe_time3
+            csvwriter.writerow([i, val, exe_time, val2, exe_time2, val3, exe_time3])
 
 if __name__ == "__main__":
     main()
